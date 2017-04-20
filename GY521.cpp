de374b8c183cfac32b9377c2e8219d5bf91d5818
Issei Kuzumaki <Issei Kuzumaki>
2017-04-20 12:22:06 +0900

    initialize and read acceleration

---
+++ GY521.cpp
@@ -0,0 +1,36 @@
+#include "GY521.h"
+#define MPU 0x68
+
+#include <Wire.h>
+
+int get_value(int addr){
+  Wire.beginTransmission(0x68);
+  Wire.write(addr);
+  Wire.endTransmission(false);
+  Wire.requestFrom(0x68,14,true);  // request a total of 14 registers
+  int accel =Wire.read()<<8|Wire.read();
+  return accel;
+}
+
+void initialize(){
+  Wire.begin();
+  Wire.beginTransmission(MPU);
+  Wire.write(0x6B);
+  Wire.write(0);
+  Wire.endTransmission(true);
+}
+
+int acceleration_z(){
+  return get_value(0x3F);
+}
+
+int acceleration_x(){
+  return get_value(0x3B);
+}
+
+
+int acceleration_y(){
+  return get_value(0x3D);
+}
+
+
---
+++ GY521.h
@@ -0,0 +1,10 @@
+#ifndef _GY521_h
+#define _GY521_h
+
+void initialize();
+
+int acceleration_x();
+int acceleration_y();
+int acceleration_z();
+
+#endif
